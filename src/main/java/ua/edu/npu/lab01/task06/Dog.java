999ad5815239555454c3b4cce4df2a58a5e30928
derbeee <19fi.o.derbenov@std.npu.edu.ua>
2020-11-26 19:55:29 +0200

    Add solved task06 for lab01

---
+++ src/main/java/ua/edu/npu/lab01/task06/Dog.java
@@ -0,0 +1,18 @@
+package ua.edu.npu.lab01.task06;
+
+public class Dog {
+    int age;
+    String name;
+    public Dog() {
+        age = 0;
+        name = "незнайомець";
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+    }
+    public Dog(int age, String name) {
+        age =age;
+        name =name;
+        System.out.println("name: "+ name);
+        System.out.println("age: "+ age);
+    }
+}
