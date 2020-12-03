6052bc850a521d5e87ee3356fedd72d31d016ab4
derbeee <19fi.o.derbenov@std.npu.edu.ua>
2020-12-03 20:18:03 +0200

    Add solved task 01 for lab 02

---
+++ src/main/java/ua/edu/npu/lab02/task01/Flying.java
@@ -0,0 +1,5 @@
+package ua.edu.npu.lab02.task01;
+
+public interface Flying {
+    String fly();
+}
---
+++ src/main/java/ua/edu/npu/lab02/task01/Walking.java
@@ -0,0 +1,5 @@
+package ua.edu.npu.lab02.task01;
+
+public interface Walking {
+    String walking();
+}
