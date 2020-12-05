20225c97b9abd42796b12c0faa266a23f6c41631
derbeee <19fi.o.derbenov@std.npu.edu.ua>
2020-12-05 07:46:21 +0200

    Add solved task 03 for lab 02

---
+++ src/main/java/ua/edu/npu/lab02/task03/Dog.java
@@ -0,0 +1,9 @@
+package ua.edu.npu.lab02.task03;
+
+public class Dog extends Pet {
+    public static void main(String[] args) {
+        Pet dog1 = new Pet("яко", 12);
+        Pet dog2 = new Pet();
+    }
+}
+
